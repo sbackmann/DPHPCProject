bc2f6a0c5f317568f1ee1328baa6f8e372e51533
ealnuaimi <ealnuaimi@student.ethz.ch>
2023-11-21 11:44:32 +0100

    fixed errors in GPU LU

--- benchmarks/lu/lu_naive_gpu.cu
+++ benchmarks/lu/lu_naive_gpu.cu
@@ -28,43 +28,42 @@ __global__ void lu_kernal(int N, double* A){
 }
 
 
-void init_array(int N, double A[N][N]) {
+void init_array(int N, double* A) {
+
+  double* B = (double*)malloc(N * N * sizeof(double));
 
   // create lower triangle of matrix 
   for (int i = 0; i < N; i++) {
     // initialize the lower triangle 
     for (int j = 0; j <= i; j++) {
-      A[i][j] = (double)(-j % N) / N + 1;
+      A[i * N + j] = (double)(-j % N) / N + 1;
     }
     // set upper triangle to zero 
     for (int j = i + 1; j < N; j++) {
-      A[i][j] = 0;
+      A[i * N + j] = 0;
     }
     // set elements on the diagonal to 1 
-    A[i][i] = 1;
+    A[i * N + i] = 1;
   }
-
-  double (*B)[N][N];
-  B = (double(*)[N][N])malloc(N*N* sizeof(double));
-
-  for (int r = 0; r < N; ++r)
-    for (int s = 0; s < N; ++s)
-      (*B)[r][s] = 0;
 
   // multiply A by A^T and save the result in B
   // result is a symmetric matrix 
   for (int t = 0; t < N; ++t)
     for (int r = 0; r < N; ++r)
       for (int s = 0; s < N; ++s)
-        (*B)[r][s] = (*B)[r][s] + (A[r][t] * A[s][t]);
+        B[r * N + s] = B[r * N + s] + (A[r * N + t] * A[s * N + t]);
 
+  // Copy the result back to A
   for (int r = 0; r < N; ++r)
     for (int s = 0; s < N; ++s)
-      A[r][s] = (*B)[r][s];
+      A[r * N + s] = B[r * N + s];
 
-  free((void*)B);
-
+  // Free the dynamically allocated memory for A and B
+  free(B);
 }
+
+
+
 
 void run_lu_kernel(int N, double* A) {
     dim3 block(16, 16);
@@ -73,20 +72,20 @@ void run_lu_kernel(int N, double* A) {
     cudaDeviceSynchronize();
 }
 
-void run_bm(int N, int M, int K, const char* preset) {
+void run_bm(int N, const char* preset) {
 
-    double* A = (double *)malloc(N*K*sizeof(double));
+    double* A = (double *)malloc(N*N*sizeof(double));
+    double *A_d;
 
-    double *A_d, *B_d, *C_d;
-    cudaMalloc((void**) &A_d, N*K*sizeof(double));
+    cudaMalloc((void**) &A_d, N*N*sizeof(double));
    
-    init_matrices(N, A);
+    init_array(N, A);
 
     
-    cudaMemcpy((void*) A_d, (void*) A, N*K*sizeof(double), cudaMemcpyHostToDevice);
+    cudaMemcpy((void*) A_d, (void*) A, N*N*sizeof(double), cudaMemcpyHostToDevice);
     
     dphpc_time3(
-       cudaMemcpy((void*) A_d, (void*) A, N*K*sizeof(double), cudaMemcpyHostToDevice),
+       cudaMemcpy((void*) A_d, (void*) A, N*N*sizeof(double), cudaMemcpyHostToDevice),
        run_lu_kernel(N, A_d),
         preset
     );
