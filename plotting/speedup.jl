8d7782ee7ef6dee73e5af7b93d3e77297779ea63
Zhurgut <dcamenisch98gmail.com>
2023-11-21 16:01:49 +0100

    separate cpu and gpu plots

--- plotting/speedup.jl
+++ plotting/speedup.jl
@@ -18,40 +18,83 @@ end
 # assumes there exists a C version called "naive"
 
 
-plot_versions(plot, df, version_apdx, color) = bar!(plot, 
+plot_versions(plot, df, version_apdx, color; label=df[1, :language]) = bar!(plot, 
     df.version .* version_apdx, df.speedup, 
     yerror=(df.speedup-df.speedup_lb, df.speedup_ub-df.speedup), 
     color=color, 
-    label=df[1, :language], 
+    label=label, 
     bar_width=0.7
 )
 
-function add_speedup(df, base)
+function add_speedup!(df, base)
     df.speedup = base ./ df.median
     df.speedup_lb = base ./ df.median_ub
     df.speedup_ub = base ./ df.median_lb
 end
 
 function make_plot(bm::String, preset::String; collect=false)
+    cd(@__DIR__)
+    cd("..")
     if collect
-        cd(@__DIR__)
-        cd("..")
         run(`julia run_benchmarks.jl -b$(bm) -p$(preset) -lcjp`)
     end
     df = CSV.read("results.csv", DataFrame)
-    grouped = groupby(df, [:benchmark, :language, :preset])
-    julia_versions = grouped[(bm, "julia",preset)]
-    c_versions = grouped[(bm, "C",preset)]
-    python_versions = grouped[(bm, "python",preset)]
+    grouped = groupby(df, [:benchmark, :language, :preset, :gpu])
 
-    ref_time = c_versions[findfirst(v->v=="naive", c_versions.version), :median]
-    add_speedup(c_versions, ref_time)
-    add_speedup(julia_versions, ref_time)
-    add_speedup(python_versions, ref_time)
+    julia_versions      = grouped[(bm, "julia", preset, false)]
+    c_versions          = grouped[(bm, "C",     preset, false)]
+    python_versions     = grouped[(bm, "python",preset, false)]
+    julia_gpu_versions  = grouped[(bm, "julia", preset, true)]
+    c_gpu_versions      = grouped[(bm, "C",     preset, true)]
+    python_gpu_versions = grouped[(bm, "python",preset, true)]
+
+    versions = [julia_versions, c_versions, python_versions, julia_gpu_versions, c_gpu_versions, python_gpu_versions]
+    c_naive = c_versions[[findfirst(v->v=="naive", c_versions.version)], :]
+    ref_time = c_naive[1, :median]
+
+    for v in versions
+        add_speedup!(v, ref_time)
+    end
     
-    p = bar(title="Speedup $bm, preset '$preset'", ylabel="Speedup", xlabel="version", xrotation=45)
+    c_naive = c_versions[[findfirst(v->v=="naive", c_versions.version)], :]
+
+    python_blue = RGBA(48/255, 101/255, 166/255)
+    julia_green = RGBA(23/255, 145/255, 62/255)
+
+    cd(@__DIR__)
+    
+    p = bar(
+        title="Speedup $bm, preset '$preset', CPU", 
+        ylabel="Speedup", 
+        xlabel="version", 
+        xrotation=45,
+        ylims=(0, 1.1*max(
+            maximum(c_versions.speedup_ub),
+            maximum(julia_versions.speedup_ub),
+            maximum(python_versions.speedup_ub),
+        ))
+    )
     plot_versions(p, c_versions, " (C)", :gray)
-    plot_versions(p, julia_versions, " (J)", :green)
-    plot_versions(p, python_versions, " (P)", :blue)
+    plot_versions(p, julia_versions, " (J)", julia_green)
+    plot_versions(p, python_versions, " (P)", python_blue)
     display(p)
+    savefig(p, "plots/speedup_$(bm)_$(preset)_cpu.pdf")
+
+    p = bar(
+        title="Speedup $bm, preset '$preset', GPU", 
+        ylabel="Speedup", 
+        xlabel="version", 
+        xrotation=45,
+        ylims=(0, 1.1*max(1, 
+            maximum(c_gpu_versions.speedup_ub),
+            maximum(julia_gpu_versions.speedup_ub),
+            maximum(python_gpu_versions.speedup_ub),
+        ))
+    )
+    plot_versions(p, c_naive, " (C CPU)", :white, label="C CPU")
+    plot_versions(p, c_gpu_versions, " (C)", :gray)
+    plot_versions(p, julia_gpu_versions, " (J)", julia_green)
+    plot_versions(p, python_gpu_versions, " (P)", python_blue)
+    display(p)
+    savefig(p, "plots/speedup_$(bm)_$(preset)_gpu.pdf");
 end
