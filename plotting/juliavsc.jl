bff4a7b452f053e6cd268b0cb088a1c306e4176c
Zhurgut <dcamenisch98@gmail.com>
2023-12-17 20:09:32 +0100

    plots julia vs c

---
+++ plotting/juliavsc.jl
@@ -0,0 +1,116 @@
+using CSV, DataFrames, Plots
+
+include("../timing/collect_measurements.jl")
+
+function collect_version(presets::Vector{String}, version::String)
+    bms = get_benchmarks()
+    for bm in bms
+        collect_measurements([bm], [:julia, :C], presets, version)
+    end
+end
+
+collect_naive(presets::Vector) = collect_version(presets, "naive")
+collect_naive(preset::String) = collect_version([preset], "naive")
+
+collect_naive_gpu(presets::Vector) = collect_version(presets, "naive_gpu")
+collect_naive_gpu(preset::String) = collect_version([preset], "naive_gpu")
+
+# you business to collect the other versions...
+
+
+
+ticks(min, max, num) = round.([min + f*(max - min) for f=0.0:1/(num-1+1e-4):1.0+0.5/(num-1+1e-4)], digits=1)
+
+function plot_julia_vs_c(bms, c_times, julia_times, name, preset)
+    percentage_speedup =  100 .* (c_times .- julia_times) ./ c_times
+
+    s = sortperm(percentage_speedup, rev=true)
+    percentage_speedup = percentage_speedup[s]
+    bms = bms[s]
+
+    mn, mx = (x->(minimum(x),maximum(x)))(percentage_speedup)
+    mn = min(0, mn)
+    mx = max(0, mx)
+
+    faster = percentage_speedup .>= 0
+    slower = percentage_speedup .<  0
+
+    total_ticks = 7
+    faster_ticks = mx / (mx - mn) * total_ticks |> round |> Int
+    slower_ticks = total_ticks - faster_ticks + 1
+
+    yticks = [ticks(0, mn, slower_ticks); ticks(0, mx, faster_ticks)]
+
+    alpha = 0.9
+    green = RGBA(23/255, 145/255, 62/255, alpha)
+    red   = RGBA(232/255, 16/255, 38/255, alpha)
+    colors = [fill(green, sum(faster)); fill(red, sum(slower))]
+
+    # plot faster ones in green
+    P = bar(
+        bms, percentage_speedup, 
+        yticks=(yticks, string.(yticks).*"%"),
+
+        title="Speedup: Julia over C, $name versions, preset $preset",
+        ylabel="Speedup vs C",
+        color=colors,
+        legend=false,
+        ylims=[1.1*min(mn, -9), 1.1*max(mx, 9)],
+    )
+
+    display(P)
+    cd(@__DIR__)
+    savefig(P, "plots/julia_vs_c_$(name)_$(preset).pdf")
+end
+
+
+function make_plot(preset::String, version::String, gpu::Bool)
+    cd(@__DIR__)
+    cd("..")
+    df = CSV.read("results.csv", DataFrame)
+    grouped = groupby(df, [:language, :preset, :gpu])
+
+    c_versions = grouped[("C", preset, gpu)]
+    julia_versions = grouped[("julia", preset, gpu)]
+
+    c_versions = c_versions[c_versions[:, "version"] .== version, :]
+    julia_versions = julia_versions[julia_versions[:, "version"] .== version, :]
+
+    c_versions = c_versions[:, ["benchmark", "median"]]
+    julia_versions = julia_versions[:, ["benchmark", "median"]]
+
+    both = innerjoin(c_versions, julia_versions, on="benchmark", renamecols="_c"=>"_julia")
+    both = both[both[!, "benchmark"] .!= "example", :]
+
+    plot_julia_vs_c(both[:, "benchmark"], both[:, "median_c"], both[:, "median_julia"], version, preset)
+end
+
+
+# use the best version
+function make_plot(preset::String, gpu::Bool)
+    cd(@__DIR__)
+    cd("..")
+    df = CSV.read("results.csv", DataFrame)
+
+    grouped = groupby(df, [:language, :preset, :gpu])
+
+    c_versions = grouped[("C", preset, gpu)]
+    julia_versions = grouped[("julia", preset, gpu)]
+
+    c_grouped = groupby(c_versions, :benchmark)
+    best_c = combine(c_grouped, :median => minimum => :best)
+    c_versions = innerjoin(c_versions, best_c, on=["benchmark", "median" => "best"])
+
+    julia_grouped = groupby(julia_versions, :benchmark)
+    best_julia = combine(julia_grouped, :median => minimum => :best)
+    julia_versions = innerjoin(julia_versions, best_julia, on=["benchmark", "median" => "best"])
+
+    c_versions = c_versions[:, ["benchmark", "median"]]
+    julia_versions = julia_versions[:, ["benchmark", "median"]]
+
+    both = innerjoin(c_versions, julia_versions, on="benchmark", renamecols="_c"=>"_julia")
+    both = both[both[!, "benchmark"] .!= "example", :]
+
+    plot_julia_vs_c(both[:, "benchmark"], both[:, "median_c"], both[:, "median_julia"], "best$(gpu ? " gpu" : "")", preset)
+
+end
