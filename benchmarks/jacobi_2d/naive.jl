864c58cc775d4ba277e472dac8577daab076460f
Zhurgut <dcamenisch98gmail.com>
2023-12-20 22:54:01 +0100

    rename jaboci for compatibility with NPBench

--- benchmarks/jacobi_2d/naive.jl
+++ benchmarks/jacobi_2d/naive.jl
@@ -14,19 +14,13 @@ end
 
 function kernel_j2d(tsteps, n, A, B)
     for t in 1:tsteps
-        for j in 2:3
-            for i in 2:(n-1)
+        for i in 2:(n-1)
+            for j in 2:(n-1)
                 B[i, j] = 0.2 * (A[i, j] + A[i, j-1] + A[i, j+1] + A[i+1, j] + A[i-1, j]);
             end
         end
-        for j in 4:(n-1)
-            for i in 2:(n-1)
-                B[i, j] = 0.2 * (A[i, j] + A[i, j-1] + A[i, j+1] + A[i+1, j] + A[i-1, j]);
-                A[i, j-2] = 0.2 * (B[i, j-2] + B[i, j-3] + B[i, j-1] + B[i+1, j-2] + B[i-1, j-2]);
-            end
-        end
-        for j in (n-2):(n-1)
-            for i in 2:(n-1)
+        for i in 2:(n-1)
+            for j in 2:(n-1)
                 A[i, j] = 0.2 * (B[i, j] + B[i, j-1] + B[i, j+1] + B[i+1, j] + B[i-1, j]);
             end
         end
@@ -37,22 +31,22 @@ end
 function main()
     tsteps, n = 50, 150
     A, B = init_arrays(n)
-    res = @dphpc_time(init_arrays(n), kernel_j2d(tsteps, n, A, B), "S")
+    res = @dphpc_time((A,B)=init_arrays(n), kernel_j2d(tsteps, n, A, B), "S")
     println(res)
 
     tsteps, n = 80, 350
     A, B = init_arrays(n)
-    res = @dphpc_time(init_arrays(n), kernel_j2d(tsteps, n, A, B), "M")
+    res = @dphpc_time((A,B)=init_arrays(n), kernel_j2d(tsteps, n, A, B), "M")
     println(res)
 
     tsteps, n = 200, 700
     A, B = init_arrays(n)
-    res = @dphpc_time(init_arrays(n), kernel_j2d(tsteps, n, A, B), "L")
+    res = @dphpc_time((A,B)=init_arrays(n), kernel_j2d(tsteps, n, A, B), "L")
     println(res)
 
     tsteps, n = 1000, 2800
     A, B = init_arrays(n)
-    res = @dphpc_time(init_arrays(n), kernel_j2d(tsteps, n, A, B), "paper")
+    res = @dphpc_time((A,B)=init_arrays(n), kernel_j2d(tsteps, n, A, B), "paper")
     println(res)
 end
 
